aacb7b3b373e4a6369fae1a4e67aa93d78672cd9
Matthew Taylor <m.taylor@7city.com>
2013-08-05 16:34:31 +0100

    Changing specific date format validator to a broad formatted dateTimeZone validator - allowing you to pass in the format and timezone when assigning properties to a document.

--- src/classes/validator/formattedtimezonedate.php
+++ src/classes/validator/formattedtimezonedate.php
@@ -29,12 +29,26 @@
  * @version $Revision$
  * @license http://www.gnu.org/licenses/lgpl-3.0.txt LGPL
  */
-class phpillowDateISO8601Validator extends phpillowValidator
+class phpillowFormattedTimeZoneDateValidator extends phpillowValidator
 {
+    private $timezone='UTC';
+    private $format=DATE_ISO8601;
+
+    public function __construct($timezone=false, $format=false)
+    {
+        if($timezone!==false) {
+            $this->timezone = $timezone;
+        }
+        if($format!==false) {
+            $this->format = $format;
+        }
+
+    }
+
     /**
      * Validate input as string
-     * 
-     * @param mixed $input 
+     *
+     * @param mixed $input
      * @return string
      */
     public function validate( $input )
@@ -44,14 +58,16 @@ class phpillowDateISO8601Validator extends phpillowValidator
         if ( is_numeric( $input ) )
         {
             $date = new DateTime( '@' . $input );
-            return $date->format( DATE_ISO8601 );
+            $date->setTimezone(new DateTimeZone($this->timezone));
+            return $date->format( $this->format );
         }
 
         // Otherwise we received most presumably some phpillowrary string, which
         // we first just try to parse with datetime (strtotime).
         if ( ( $date = new DateTime( $input ) ) !== false )
         {
-            return $date->format( DATE_ISO8601 );
+            $date->setTimezone(new DateTimeZone($this->timezone));
+            return $date->format( $this->format );
         }
 
         // If DateTime could not parse the string, we got a problem. Maybe
@@ -59,8 +75,8 @@ class phpillowDateISO8601Validator extends phpillowValidator
         //
         // Since PHP 5.3 datetime seems to accept everything and just returns
         // NOW, if it fails to parse. So this seems untestable for now.
-        throw new phpillowValidationException( 
-            'Error parsing the date: %date', 
+        throw new phpillowValidationException(
+            'Error parsing the date: %date',
             array(
                 'date' => $input,
             )
